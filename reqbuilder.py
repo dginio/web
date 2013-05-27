0de125c3a273374bbd1cb3b3b8b79fa61accfeea
dginio <god@dginio.fr>
2013-05-28 01:08:53 +0200

    New features

--- reqbuilder.py
+++ reqbuilder.py
@@ -4,6 +4,10 @@
 import urllib, urllib2, os, time, gzip
 from scapy.all import *
 
+dashs = "-"*80
+
+http_timeout = 5
+
 count = 0
 requests = []
 
@@ -18,10 +22,11 @@ def find_host(request) :
 
 def find_method(request) :
 	try :
-		return re.compile('(^[^\ ]*\ )').findall(request)[0].strip()
+		return re.compile('^([^\ ]+\ )').findall(request)[0].strip()
 	except :
 		return ""
 		pass
+		
 def find_uri(request) :
 	try :
 		return re.compile('(\ [^\ ]*\ )').findall(request)[0].strip()
@@ -33,71 +38,102 @@ def list_requests(requests) :
 	i = 0
 	for request in requests :
 		i += 1
-		print str(i)+" "+find_method(request)+" "+find_host(request)+find_uri(request)
+		print str(i)+" "+find_method(request)+" http://"+find_host(request)+find_uri(request)
 
-def replay(request) :
+def give_filename(filename,ext) :
+	basename = filename.replace("www.","")
+	tmp = basename
+	i = 0
+	while os.path.isfile(tmp+"."+ext) :
+		i+=1
+		tmp=basename+str(i)
+	return tmp+"."+ext
+
+def play(request) :
 	url = "http://"+find_host(request)+find_uri(request)
 	new_request = urllib2.Request(url)
-	
+
 	post = 0
-	
-	for line in request.split('\n') :
+
+	for line in request.replace("\r","\n").split("\n") :
 		if ":" in line :
 			splited = line.strip().split(":", 1)
 			if "Host" not in splited[0]:
 				new_request.add_header(splited[0].strip(), splited[1].strip())
+		elif line.strip() == "" and find_method(request) == "POST" :
+			post = 1
 		elif post :
 			data = line
-		else :
-			if line.strip() == "" and find_method(request) == "POST" :
-				post = 1
-	try : 
-		if post : reply = urllib2.urlopen(new_request,data)
-		else : reply = urllib2.urlopen(new_request)
+			break
 	
-		filename_base = re.compile('(.*)\.[^\.]*').findall(find_host(request))[0].strip()
-		filename = filename_base
-		i = 0
-		while os.path.isfile(filename) :
-			i+=1
-			filename=filename_base+str(i)
-		
+	out = ""
+	
+	try :
+		print "Sending request..."
+		if post : reply = urllib2.urlopen(new_request,data,timeout = http_timeout)
+		else : reply = urllib2.urlopen(new_request,timeout = http_timeout)
+		out = reply.read()
+	except urllib2.HTTPError, e:
+		print "Error HTTP, code : ", e.code
+		if e.code == 304 :
+			lines = ""
+			for line in request.replace("\r","\n").split("\n") :
+				if "If-Modified-Since: " in line :
+					lines += "\n  "+line
+					break
+			for line in request.replace("\r","\n").split("\n") :
+				if "If-None-Match: " in line :
+					lines += "\n  "+line
+					break
+			if lines : print "Remove these lines in the request to disable last modification detection : "+lines
+	except urllib2.URLError, e:
+		print "can't reach a server."
+		print "Reason: ", e.reason
+
+	if out :
+		filename = give_filename(re.compile('(.*)\.[^\.]*').findall(find_host(request))[0].strip(),"out")
+
 		newfile = open(filename,"w")
-		newfile.write(reply.read())
+		newfile.write(out)
 		newfile.close()
-	
-		print filename+" enregistré avec succès."
-		
+
 		typefile = execute("file "+filename)
-		
-		print "File : "+typefile+"\n"
-		
+
+		print "File "+filename+" saved.\n"+typefile
+
 		if "gzip" in typefile :
-			choice = raw_input("uncompress ? (o/n)")
-			if "o" in choice :
-				newfile = open(filename+"_uncompress","w")
-				newfile_gz = gzip.open(filename, 'rb')
-				newfile.write(newfile_gz.read())
-				newfile_gz.close()
+			for line in request.replace("\r","\n").split("\n") :
+				if "Accept-Encoding: " in line :
+					print "Remove this line in the request to disable gzip compression : \n  "+line
+					break
+			choice = raw_input("\nDecompress gzip now (y/n) ? ")
+			if "y" in choice :				
+				newfile = gzip.open(filename,"rb")
+				out = newfile.read()
 				newfile.close()
-	
-			print filename+"_uncompress enregistré avec succès."
-	except :
-		print "Erreur HTTP"
-		pass
+				newfile = open(filename,"w")
+				newfile.write(out)
+				newfile.close()
+				typefile = execute("file "+filename)
+				print "\nFile "+filename+" saved.\n"+typefile
 
 	menu(request)
 
-def export(request) :
+def export_raw(request) :
+	filename = give_filename("newreq_"+re.compile('(.*)\.[^\.]*').findall(find_host(request))[0].strip(),"raw")
+	newfile = open(filename,"w")
+	newfile.write(request)
+	newfile.close()
+	
+	print "File "+filename+" saved.\n"
+	
+	menu(request)
+
+def export_python(request) :
 	url = "http://"+find_host(request)+find_uri(request)
 	
-	filename_base = "request_"+re.compile('(.*)\.[^\.]*').findall(find_host(request))[0].strip().replace("www.","")+".py"
-	filename = filename_base
-	i = 0
-	while os.path.isfile(filename) :
-		i+=1
-		filename=str(i)+filename_base
-		
+	filename = give_filename("newreq_"+re.compile('(.*)\.[^\.]*').findall(find_host(request))[0].strip(),"py")
+
 	newfile = open(filename,"w")
 	newfile.writelines([
 	'#!/usr/bin/python\n',
@@ -105,82 +141,166 @@ def export(request) :
 	'import urllib, urllib2\n\n',
 	'request = urllib2.Request("'+url+'")\n\n',
 	])
-	
+
 	post = 0
-	
+
 	for line in request.split('\n') :
 		if ":" in line :
 			splited = line.strip().split(":", 1)
 			if "Host" not in splited[0]:
 				newfile.write('request.add_header("'+splited[0].strip()+'", "'+splited[1].strip().replace('"','')+'")\n')
+		elif line.strip() == "" and find_method(request) == "POST" :
+			post = 1
 		elif post :
 			data = line
-		else :
-			if line.strip() == "" and find_method(request) == "POST" :
-				post = 1
-				
-	if post : newfile.write('\nreply = urllib2.urlopen(request,"'+data.strip()+'")\n')
-	else : newfile.write('\nreply = urllib2.urlopen(request)\n')
+			break
+
+	if post : newfile.write('\nreply = urllib2.urlopen(request,"'+data.strip()+'",timeout = '+str(http_timeout)+')\n')
+	else : newfile.write('\nreply = urllib2.urlopen(request,timeout = '+str(http_timeout)+')\n')
 	newfile.write('\nprint reply.read()\n\n')
 	newfile.close()
 
-	print filename+" créé."
+	print "File "+filename+" saved.\n"
 
+	menu(request)
+	
+def export_bash(request) :
+	url = "http://"+find_host(request)+find_uri(request)
+	
+	filename = give_filename("newreq_"+re.compile('(.*)\.[^\.]*').findall(find_host(request))[0].strip(),"sh")
+
+	wget = "wget "	
+	
+	post = 0
+
+	for line in request.split('\n') :
+		if ":" in line :
+			splited = line.strip().split(":", 1)
+			if "Host" not in splited[0]:
+				wget += "--header='"+splited[0].strip()+": "+splited[1].strip().replace("'","")+"' "
+		elif line.strip() == "" and find_method(request) == "POST" :
+			post = 1
+		elif post :
+			data = line
+			break
+
+	if post : wget += "--post-data='"+data.strip()+"' "
+	
+	wget += url
+	
+	newfile = open(filename,"w")
+	newfile.writelines(['#!/bin/bash\n\n',wget+'\n'])
+	newfile.close()
+
+	print "File "+filename+" saved.\n"
+
+	menu(request)
+
+def modify(request) :
+	newfile = open("reqbuilder.tmp","w")
+	newfile.write(request)
+	newfile.close()
+	os.system("nano reqbuilder.tmp")
+	newfile = open("reqbuilder.tmp","r")
+	newrequest = newfile.read()
+	newfile.close()
+	os.system("rm reqbuilder.tmp")
+	
+	os.system("clear")
+	
+	if newrequest in request :
+		print "Nothing changed."
+	else : 
+		request = newrequest
+		print "Modifications applied."
+	
+	print dashs
+	print request
 	menu(request)
 
 def select() :
 	global requests
 	os.system("clear")
-	print "------------------------------------------------------------"
+	print dashs
 	list_requests(requests)
-	print  "------------------------------------------------------------"
-	req = raw_input('Sélectionner la requête : ')
+	print dashs
+	req = raw_input('Select a request : ')
 	request = requests[int(req)-1]
-	print "------------------------------------------------------------"
+	print dashs
 	print request
 	menu(request)
-	
 
+def pcap() :
+	global requests, count
+	
+	path = ""
+	while not os.path.isfile(path) :
+		if path : print "No such file"
+		path = raw_input("path of the pcap file : ")
+    
+	count = 0
+	requests = []	
+	
+	packets = rdpcap(path)
+    
+	for packet in packets :
+		if check(packet) :
+			handle(packet)
+	print
+	select()
+	
 def menu(request) :
-	print "------------------------------------------------------------"
+	print dashs
 	print """
 		--- Request Builder ---
 
-	[+] 1 - Relancer la capture
-	[+] 2 - Sélectionner une autre requête
-	[+] 3 - Rejouer la requête et enregistrer le résultat
-	[+] 4 - Exporter la requête en python
-	[+] 5 - Quitter
+	[+] 1 - Restart sniffind
+	[+] 2 - Open a pcap file
+	[+] 3 - Select another request
+	[+] 4 - Modify the current request
+	[+] 5 - Play the current request and save the result
+	[+] 6 - Export the current request in raw format
+	[+] 7 - Export the current request in a python script
+	[+] 8 - Export the current request in a bash script
+	[+] 0 - Exit
 	"""	
-	choix = raw_input("Sélection : ")
+	choix = input("Choice : ")
 	print
-	if choix > "4":
-		exit()
-	else :		
-		if choix == "1": 
-			main()		
-		elif choix == "2": 
-			select()
-		elif choix == "3":
-			replay(request)
-		elif choix == "4":
-			export(request)
+	if   choix <  1 : exit()
+	elif choix == 1 : main()
+	elif choix == 2 : pcap()
+	elif choix == 3 : select()
+	elif choix == 4 : modify(request)
+	elif choix == 5 : play(request)
+	elif choix == 6 : export_raw(request)
+	elif choix == 7 : export_python(request)
+	elif choix == 8 : export_bash(request)
 
 def handle(p) :
-	paquet  = p[Raw].load
-	if "HTTP" in paquet :
-		global count, requests
-		count += 1
-		print str(count)+" "+find_method(paquet)+" http://"+find_host(paquet)+find_uri(paquet)
-		requests.append(paquet)
+	packet  = p[Raw].load
+	global count, requests
+	count += 1
+	print str(count)+" "+find_method(packet)+" http://"+find_host(packet)+find_uri(packet)
+	requests.append(packet)
+
+def check(p) :
+	if p.haslayer(TCP) and p.haslayer(Raw):
+		try :
+			if any( find_method(p[Raw].load) == test for test in ["GET","HEAD","POST","OPTIONS","CONNECT","TRACE","PUT","PATCH","DELETE"] ) :
+				return p
+		except :
+			pass
 
 def main() :
 	try :
-		global requests
-		requests = []
+		global requests, count
+		count = 0
+		requests = []		
 		print "Sniffing HTTP"
-		n = input("Nombre de paquets HTTP à capturer :")
-		sniff( count = n, store = 0, filter = "tcp and dst port 80", lfilter = lambda(p): p.haslayer(TCP) and p.haslayer(Raw), prn = handle )
+		n = input("Number of requests to sniff : ")
+		os.system("clear")
+		print dashs
+		sniff( count = n, store = 0, filter = "tcp", lfilter = check, prn = handle )
 		select()
 	except (KeyboardInterrupt):
 		select()
